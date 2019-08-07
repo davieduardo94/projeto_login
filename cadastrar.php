8a0c8ad02d8635189af30fb35b0b96b246eb8efb
Davi Eduardo <davieduardo94@gmail.com>
2019-08-07 17:42:32 -0400

    Comit two
    arquivos de cadastro e  processamento de informaçoes. / files of sign in and process.

---
+++ cadastrar.php
@@ -0,0 +1,65 @@
+<?php  
+require_once 'classes/usuarios.php';
+$u = new Usuario;
+?>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet"  href="css/estilo.css">
+</head>
+<body>
+<div id="corpo-form-cad">
+	<h1>Cadastre-se</h1>
+	<form method="POST" action="processa.php">
+		<input type="text" name="nome" placeholder="Nome Completo" maxlength="45">
+		<input type="text" name="telefone" placeholder="Telefone" maxlength="30">
+		<input type="email" name="email" placeholder="Usuario" maxlength="40">
+		<input type="password" name="senha" placeholder="Senha" maxlength="20">
+		<input type="password" name="confSenha" placeholder="Confirmar senha">
+		<input type="submit" value="CADASTRAR" class="entrar">
+	</form>
+<?php
+//verificar se clicou no botao
+if(isset($_POST['nome']))
+{
+	$nome = addslashes($_POST['nome']); //addslashes evita codigos maliciosos.
+	$telefone = addslashes($_POST['telefone']);
+	$email = addslashes($_POST['email']);
+	$senha = addslashes($_POST['senha']);
+	$confirmarSenha = addslashes( $_POST['confSenha']);
+	//verificando se todos os campos nao estao vazios
+	if(!empty($nome) && !empty($telefone) && !empty($email) && !empty($senha) && !empty($confirmarSenha)) 
+	{
+		$u->conectar("projeto_login","localhost","root","");
+		if ($u->msgErro == "") //conectado normalmente;
+		{
+			if ($senha == $confirmarSenha) 
+			{
+				if ($u->cadastrar($nome, $telefone, $email, $senha)) 
+				{
+					echo "Cadastro realizado com sucesso!";
+				}
+				else
+			 	{
+			 		echo "Email já cadastrado, retorne e faça login.";
+			 	}
+			}
+			else
+			{
+				echo "Senhas não conferem!";
+			}
+		}
+		else 
+		{
+		echo "Erro: ".$u->msgErro;
+		}
+	}
+	else
+		{
+			echo "Preencha todos os campos!";
+		}
+}
+?>
+</div>
+</body>
+</html>
---
+++ processa.php
@@ -0,0 +1,44 @@
+<?php  
+require_once 'classes/usuarios.php';
+$u = new Usuario;
+//verificar se clicou no botao
+if(isset($_POST['nome']))
+{
+	$nome = addslashes($_POST['nome']); //addslashes evita codigos maliciosos.
+	$telefone = addslashes($_POST['telefone']);
+	$email = addslashes($_POST['email']);
+	$senha = addslashes($_POST['senha']);
+	$confirmarSenha = addslashes( $_POST['confSenha']);
+	//verificando se todos os campos nao estao vazios
+	if(!empty($nome) && !empty($telefone) && !empty($email) && !empty($senha) && !empty($confirmarSenha)) 
+	{
+		$u->conectar("projeto_login","localhost","root","");
+		if ($u->msgErro == "") //conectado normalmente;
+		{
+			if ($senha == $confirmarSenha) 
+			{
+				if ($u->cadastrar($nome, $telefone, $email, $senha)) 
+				{
+					echo "Cadastro realizado com sucesso!";
+				}
+				else
+			 	{
+			 		echo "Email já cadastrado, retorne e faça login.";
+			 	}
+			}
+			else
+			{
+				echo "Senhas não conferem!";
+			}
+		}
+		else 
+		{
+		echo "Erro: ".$u->msgErro;
+		}
+	}
+	else
+		{
+			echo "Preencha todos os campos!";
+		}
+}
+?>
