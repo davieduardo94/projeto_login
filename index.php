ca35c069a710a81ec1f64e8b27f35928713da300
Davi Eduardo <davieduardo94@gmail.com>
2019-08-07 17:40:35 -0400

    comit primeira pagina.
    Pagina apenas em HTML, apontando para o CSS que define os layouts iniciais.

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet"  href="css/estilo.css">
+</head>
+<body>
+<div id="corpo-form">
+	<h1>Entrar</h1>
+	<form method="POST" action="processa.php">
+		<input type="email" placeholder="Usuario">
+		<input type="password" placeholder="Senha">
+		<input type="submit" value="ACESSAR" class="entrar">
+		<a href="cadastrar.php">Ainda não é inscrito? <strong>Cadastre-se</strong></a>
+	</form>
+</div>
+</body>
+</html>
