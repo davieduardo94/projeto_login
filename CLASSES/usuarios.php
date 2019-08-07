eae81cf79ab34404e508de56c6d09fa697f353f2
Davi Eduardo <davieduardo94@gmail.com>
2019-08-07 17:47:04 -0400

    updating class users
    Class with informations of connect, login, sign in  and session.

--- CLASSES/usuarios.php
+++ CLASSES/usuarios.php
@@ -1,1 +1,66 @@
+<?php
+ Class Usuario {
+	private $pdo;  /*criando variavel para usar nas funçoes*/
+	public $msegErro = "";
+  	public function conectar($bdnome, $host, $usuario, $senha)
+  	{
+  		global $pdo;
+  		try 
+  		{
+  			$pdo = new PDO("mysql:dbname=".$bdnome.";host=".$host, $usuario, $senha);
+  		} catch (PDOException $e) {
+  			$msgErro - $e->getMessage(); /*pega a mensagem de erro do php e joga na variavel msegErro e mostra pro usuario.*/
+  		}
+  	}
+  	public function cadastrar($nome, $telefone, $email, $senha)
+  	{
+  		global $pdo;
+  		//verificando se existe usuario cadastrado.
+  		$sql = $pdo->prepare("select id from usuario where email= :e"); //pega o id do usuario buscando pelo emial preenchido no cadastro
+  		$sql->bindValue(":e", $email);  //substitui o :e pelo email preenchido no cadastro
+  		$sql->execute();
+  		if($sql->rowCount()>0) //verificando houve resposta na consulta
+  		{
+  			return false; // ja tem cadastro
+  		}
+  		else
+  		{
+  			//caso nao tenha
+  			$sql = $pdo->prepare("insert into usuario (nome, telefone, email, senha) values (:n, ;t, :e,:s");
+	  		$sql->bindValue(":n", $nome);
+	  		$sql->bindValue(":t", $telefone);
+	  		$sql->bindValue(":e", $email);
+	  		$sql->bindValue(":s", md5($senha));
+	  		$sql->execute();
+	  		return true;
+  		}
+
+  	}
+  	public function logar($email, $senha)
+  	{
+  		global $pdo;
+  		/*verificar se o email e senha estao cadastrados, se sim*/
+  		$sql= $pdo->prepare("select id from usuarios where email = :e and senha = :s");
+  		$sql->bindValue(":e", $email);
+  		$sql->bindValue(":s", md5($senha));
+  		$sql->execute();
+  		if($sql->rowCount()>0) //verificando houve resposta na consulta
+  		{
+  			//entrar no sistema criando uma (sessao)
+  			$dado = $sql->fetch(); //transforma o retorno da query em array com os nomes das colunas
+  			session_start();  //iniciando a sessao
+  			$_SESSION['id'] = $dado['id']; //armazena o id do usuario na sessao.
+  			return true;  //logado com sucesso
+  		}
+  		else
+  		{
+  			return false; //erro ao logar.
+  		}
+  	}
+}
+?>
+
+
+
+
 
