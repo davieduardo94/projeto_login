b70199847b49bc8905feceb06f7f22f2ab557085
Davi Eduardo <davieduardo94@gmail.com>
2019-08-15 17:04:39 -0400

    private area in begin stage

--- areaprivada.php
+++ areaprivada.php
@@ -1,1 +1,11 @@
+<?php
+//verificando se a sessao existe e evitando acesso indevido.
+  session_start();
+  if (!isset($_SESSION['id'])) {  //se não está definido o id do usuario na sessao
+    header("location:index.php");
+    exit;
+  }
+?>
 
+<h1>Bem Vindo a sua area privada!!</h1>
+<a href="sair.php">Sair</a>
