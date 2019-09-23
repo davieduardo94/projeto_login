411860f18c88a31051e452a1e399b056eefb4419
Davi Eduardo <davieduardo94@gmail.com>
2019-09-23 09:06:09 -0400

    change charset and rename file

--- banco_projeto.sql
+++ banco_projeto.sql
@@ -3,5 +3,5 @@ id_usuario int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 nome varchar(30) DEFAULT NULL, 
 telefone varchar(30) DEFAULT NULL, 
 email varchar(40) DEFAULT NULL, 
-senha varchar(32) DEFAULT NULL ) 
-ENGINE=InnoDB DEFAULT CHARSET=latin1
+senha varchar(32) DEFAULT NULL )
+ENGINE=InnoDB DEFAULT CHARSET=utf8
